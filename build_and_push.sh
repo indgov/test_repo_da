3d829ed0bf08493754405b704dc0d67b2a62b7e0
govind <govind.reg.92@gmail.com>
2024-10-07 17:29:50 +1100

    Test 8

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,7 +3,6 @@
 IMAGE_NAME="vindog/da-test"
 TAG="latest"
 
-echo "${DOCKER_HUB_TOKEN}"
 docker build -t ${IMAGE_NAME}:${TAG} .
 echo "${DOCKER_HUB_TOKEN}" | docker login -u "${DOCKER_HUB_USER}" --password-stdin
 
